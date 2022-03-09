8e9a6a469218bd03264edf23324846004424750f
EcMangawang <emmari.mangawang@edu.sait.ca>
2022-03-09 14:12:51 -0700

    Delete method added

--- src/main/java/ca/sait/lab6/dataaccess/UserDB.java
+++ src/main/java/ca/sait/lab6/dataaccess/UserDB.java
@@ -21,6 +21,7 @@ public class UserDB {
         
         try {
             ps = con.prepareStatement(sql);
+            
             rs = ps.executeQuery();
             
             while (rs.next()) {
@@ -56,6 +57,7 @@ public class UserDB {
         
         try {
             ps = con.prepareStatement(sql);
+            
             ps.setString(1, email);
             rs = ps.executeQuery();
             if (rs.next()) {
@@ -88,6 +90,7 @@ public class UserDB {
         
         try {
             ps = con.prepareStatement(sql);
+            
             ps.setString(1, user.getEmail());
             ps.setString(2, user.getFirstName());
             ps.setString(3, user.getLastName());
@@ -110,38 +113,49 @@ public class UserDB {
         return inserted;
     }
 
-    public void update(User user) throws Exception {
+    public boolean update(User user) throws Exception {
         ConnectionPool cp = ConnectionPool.getInstance();
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
         String sql = "UPDATE user SET `first_name` = ?, `last_name` = ?, `password` =?, `role` = ? WHERE  `email` = ?;";
         
-        try {
-            ps = con.prepareStatement(sql);
-            ps.setString(2, user.getFirstName());
-            ps.setString(3, user.getLastName());
-            ps.setString(4, user.getPassword());
-            ps.setInt(5, user.getRole().getId());
-        } finally {
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
-        }
-    }
-
-    public void delete(Note note) throws Exception {
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        String sql = "DELETE FROM note WHERE note_id=?";
+        boolean updated;
         
         try {
             ps = con.prepareStatement(sql);
-            ps.setInt(1, note.getNoteId());
-            ps.executeUpdate();
+            
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setString(3, user.getPassword());
+            ps.setInt(4, user.getRole().getId());         
+            ps.setString(5, user.getEmail());
+            updated = ps.executeUpdate() != 0;
         } finally {
             DBUtil.closePreparedStatement(ps);
             cp.freeConnection(con);
         }
+        
+        return updated;
+    }
+
+    public boolean delete(User user) throws Exception {
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        String sql = "DELETE FROM user WHERE email=?";
+        
+        boolean deleted;
+        
+        try {
+            ps = con.prepareStatement(sql);
+            
+            ps.setString(1, user.getEmail());
+            deleted = ps.executeUpdate() != 0;
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            cp.freeConnection(con);
+        }
+        return deleted;
     }
 
 }
